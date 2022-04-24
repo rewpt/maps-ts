ad2ba4add86c2a736e17c680bcf9cc9e4d3de479
rewpt <zpermack@gmail.com>
2022-04-24 17:37:10 -0400

    add User class and import for creating a single user in index

--- src/User.ts
+++ src/User.ts
@@ -1,14 +1,28 @@
-import faker from 'faker';
+// command clicking on faker takes us to the type definition file which can give us
+// a ton of information on how to use the library, in this case better than
+// the documentation.
+import { faker } from '@faker-js/faker';
+// we need type definition files, which some libraries come with like Axios
+// newest version of faker does 
+// Old version did not so we would have needed to check 'Definitely Typed' to find it
+// We can do this at npmjs.com and query @types/faker
+// we would have then installed something like npm install @types/faker
 
 // creating our User class type annotations
-class User {
+export class User {
   name: string;
+  // this will not initialize the lat and long variables, it just lets TS know they are coming
   location: {
     lat: number;
-    long: number
+    lng: number
   }
 
   constructor( ) {
-
+    this.name = faker.name.firstName(); 
+    // Now we have to initialize the location lat and long vars
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    }
   }
 } 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('hi')
+// It is convention in TS to not use default exports so we always have curly braces
+import {User} from './User';
+
+const user = new User();
+
+console.log(user)
+
