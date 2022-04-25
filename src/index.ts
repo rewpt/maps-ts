90129062a4e295412ac08457c094223f8a7eeef2
rewpt <zpermack@gmail.com>
2022-04-24 22:52:19 -0400

    add type files for google maps and api key script

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 // It is convention in TS to not use default exports so we always have curly braces
 import {User} from './User';
+import {Company} from './Company';
 
 const user = new User();
+const company = new Company();
 
-console.log(user)
+console.log(user, company)
 
