0fa7af864892fbcbb7561ceaaffb1eed7b792b69
rewpt <zpermack@gmail.com>
2022-04-25 23:27:48 -0400

    extend company interface to use map type

--- src/Company.ts
+++ src/Company.ts
@@ -1,20 +1,31 @@
-import faker from '@faker-js/faker';
+import faker from "@faker-js/faker";
+import { Mappable } from "./CustomMap";
 
-export class Company {
+// By saying implements mappable we tie the class to this
+// interface which will give us better error messages
+// if there is an error where your class initialization is
+// not congruent with the interface
+export class Company implements Mappable {
   companyName: string;
   catchPhrase: string;
   location: {
     lat: number;
     lng: number;
-  }
+  };
 
   constructor() {
     this.companyName = faker.company.companyName();
     this.catchPhrase = faker.company.catchPhrase();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
-      lng: parseFloat(faker.address.longitude()), 
-    }
+      lng: parseFloat(faker.address.longitude()),
+    };
   }
-};
 
+  markerContent(): string {
+    return `
+    <h1>Comapny Name: ${this.companyName}</h1>
+    <h3>Catchphrase: ${this.catchPhrase}</h3>
+    `;
+  }
+}
