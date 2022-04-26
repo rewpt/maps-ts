1f1d2b4c35d92201a3ae01b8bdbfd523e2978a57
rewpt <zpermack@gmail.com>
2022-04-25 23:28:19 -0400

    extend User class  to use mappable interface

--- src/User.ts
+++ src/User.ts
@@ -1,28 +1,33 @@
 // command clicking on faker takes us to the type definition file which can give us
 // a ton of information on how to use the library, in this case better than
 // the documentation.
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
+import { Mappable } from "./CustomMap";
 // we need type definition files, which some libraries come with like Axios
-// newest version of faker does 
+// newest version of faker does
 // Old version did not so we would have needed to check 'Definitely Typed' to find it
 // We can do this at npmjs.com and query @types/faker
 // we would have then installed something like npm install @types/faker
 
 // creating our User class type annotations
-export class User {
+export class User implements Mappable {
   name: string;
   // this will not initialize the lat and long variables, it just lets TS know they are coming
   location: {
     lat: number;
-    lng: number
-  }
+    lng: number;
+  };
 
-  constructor( ) {
-    this.name = faker.name.firstName(); 
+  constructor() {
+    this.name = faker.name.firstName();
     // Now we have to initialize the location lat and long vars
     this.location = {
       lat: parseFloat(faker.address.latitude()),
-      lng: parseFloat(faker.address.longitude())
-    }
+      lng: parseFloat(faker.address.longitude()),
+    };
   }
-} 
+
+  markerContent(): string {
+    return `<h1> User Name: ${this.name}</h1>`;
+  }
+}
